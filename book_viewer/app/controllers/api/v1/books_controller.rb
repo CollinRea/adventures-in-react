4b4e2eba7321f157f9011c0a59edc53c4c5ef532
Collin Rea <collinr01@gmail.com>
2016-12-10 12:00:28 -0600

    Initial rails app setup. Added responders gem and setup Book model, migration and an API controller

---
+++ book_viewer/app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::BooksController < Api::V1::BaseController
+  def index
+    respond_with Book.all
+  end
+
+  def create
+    respond_with :api, :v1, Book.create(book_params)
+  end
+
+  def destroy
+    respond_with Book.destroy(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update_attributes(book_params)
+    respond_with book, json: book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:id, :name, :content)
+  end
+
+end
